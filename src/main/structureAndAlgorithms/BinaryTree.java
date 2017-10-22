a42c0a930125e5cda23ca2c26852b347fead0e8e
Thomas Leruth <th.leruth@gmail.com>
2017-10-21 20:44:31 -0500

    Making the binary tree self balancing

--- src/main/structureAndAlgorithms/BinaryTree.java
+++ src/main/structureAndAlgorithms/BinaryTree.java
@@ -9,14 +9,12 @@ import java.util.ArrayList;
 public class BinaryTree {
 
     binaryNode top;
-    ArrayList<Integer> list = new ArrayList<>();
 
     public BinaryTree() {
         this.top = null;
     }
 
     public void add(int i) {
-
 
         if (top == null) {
             top = new binaryNode(i);
@@ -39,18 +37,25 @@ public class BinaryTree {
                 }
             }
         }
+
+        rebalancing();
     }
 
-    public ArrayList<Integer> retrieveAll(BinaryTree tree, binaryNode top) {
+    public ArrayList<Integer> retrieveAll(binaryNode top, ArrayList<Integer> list) {
 
         binaryNode searcher = top;
 
+        if (top == null) {
+            System.out.println("The tree is empty");
+            return list;
+        }
+
         if (searcher.left != null) { // go to the left
-            retrieveAll(tree, searcher.left);
+            retrieveAll(searcher.left, list);
         }
         if (searcher.right != null) {
             list.add(searcher.value);
-            retrieveAll(tree, searcher.right);
+            retrieveAll(searcher.right, list);
         }
 
         if (searcher.right == null && searcher.left == null) {
@@ -58,6 +63,82 @@ public class BinaryTree {
         }
 
         return list;
+    }
+
+    public void removeAll() {
+
+//        while (top.left != null && top.right != null) {
+//            binaryNode hitman = top;
+//
+//            while (hitman.left != null) { // go to the left
+//                hitman = hitman.left;
+//            }
+//            while (hitman.right != null) {
+//                hitman = hitman.right;
+//            }
+//
+//            if (hitman.left == null) {
+//                hitman = null;
+//            }
+//        }
+
+        top = null;
+    }
+
+    private void rebalancing() {
+
+        double difference = 3;
+        int minimum = 10;
+
+        ArrayList<Integer> listL = new ArrayList<>();
+        ArrayList<Integer> listR = new ArrayList<>();
+
+        int sizeLeft;
+        int sizeRight;
+
+        if (top.right == null) {
+            sizeRight = 0;
+        }
+        else {
+            sizeRight = retrieveAll(top.right, listR).size();
+        }
+
+        if (top.left == null) {
+            sizeLeft = 0;
+        }
+        else {
+            sizeLeft = retrieveAll(top.left, listL).size();
+        }
+
+        System.out.println("####");
+        System.out.println("Left: " + sizeLeft);
+        System.out.println("Right: " + sizeRight);
+        if (sizeLeft > minimum || sizeRight > minimum) { // avoid dividing by 0 and small trees
+            if ((double) (sizeLeft/sizeRight) > difference || (double) (sizeLeft/sizeRight) < (1/difference)) {
+                System.out.println("Rebalancing needed");
+                ArrayList<Integer> listF = new ArrayList<>();
+                retrieveAll(top, listF); // getting the data to add back
+                removeAll();
+                addSortedArrayList(listF, 0, listF.size()-1);
+                System.out.println("Rebalancing done");
+            }
+        }
+
+    }
+
+    public void addSortedArrayList(ArrayList<Integer> list, int start, int end) {
+
+        if (start <= end) {
+            int middle = start + (end - start) / 2;
+            int b = list.get(middle);
+            add(b);
+            if (middle != start) {
+                addSortedArrayList(list, start, middle - 1);
+            }
+            if (middle != end) {
+                addSortedArrayList(list, middle + 1, end);
+            }
+        }
     }
 
 
